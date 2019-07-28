ba7657d27548d2829edac3b7d8c4d4fb37fab7d2
Kev Moturi <ketonkss4@gmail.com>
2019-07-28 19:18:59 -0400

    correct button sizing, correct fonts

--- lib/ui/pages/addresser_page.dart
+++ lib/ui/pages/addresser_page.dart
@@ -7,17 +7,55 @@ class AddresserPage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
-          margin: EdgeInsets.only(top: 20.0),
+          margin: EdgeInsets.only(top: 40.0),
           child: Center(
-            child: Text('Where do you\nwanna go?',
+            child: Text(
+              'Where do you\nwanna go?',
               textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.black,
-               fontSize: 32,
-               fontFamily: 'RobotoBold'),
-               ),
-          )
-        )
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 72.0, right: 24.0, left: 24.0),
+          child: SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: MaterialButton(
+              child: Text('Home',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700)),
+              color: Colors.cyan,
+              textColor: Colors.white,
+              elevation: 4,
+              onPressed: () {},
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 24.0, right: 24.0, left: 24.0),
+          child: SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: MaterialButton(
+              child: Text('Somewhere Else',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700)),
+              color: Colors.deepPurple,
+              textColor: Colors.white,
+              elevation: 4,
+              onPressed: () {},
+            ),
+          ),
+        ),
       ],
     );
   }
