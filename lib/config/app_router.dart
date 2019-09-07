7b94ed350fccb2dc62910818033798e862de51d6
Kev Moturi <ketonkss4@gmail.com>
2019-09-07 15:53:59 -0700

    add ride party page to app router

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -2,6 +2,7 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:gmoh_app/ui/pages/action_selection_page.dart';
 import 'package:gmoh_app/ui/pages/locater_page.dart';
+import 'package:gmoh_app/ui/pages/ride_party_page.dart';
 
 class AppRouter {
   static Router router = Router();
@@ -9,12 +10,17 @@ class AppRouter {
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
           ActionSelectionPage());
 
+  static Handler _ridePartyPageHandler = Handler(
+      handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
+          RidePartyPage());
+
   static Handler _locatorPageHandler = Handler(
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
           LocatorPage(params['isGoingHome'][0] == 'true'));
 
   static void setupRouter() {
     router.define('action_selection', handler: _actionSelectionHandler);
+    router.define('ride_party_page', handler: _ridePartyPageHandler);
     router.define('locator_page/:isGoingHome', handler: _locatorPageHandler);
   }
 }
