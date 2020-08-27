d086da62e52130ace4742197b2ec93bbb40d440f
Princeps Polycap <7855677+princepspolycap@users.noreply.github.com>
2020-08-27 13:46:55 -0700

    refactor locator_page.dart to work with multiple screens sizes (#43)

--- lib/ui/pages/locator/locator_page.dart
+++ lib/ui/pages/locator/locator_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:gmoh_app/io/apis/google_api_services.dart';
 import 'package:gmoh_app/io/repository/destinations_search_repo.dart';
@@ -11,6 +12,7 @@ import 'package:gmoh_app/ui/pages/trip_confirmation_map.dart';
 import 'package:gmoh_app/util/hex_color.dart';
 import 'package:gmoh_app/util/remote_config_helper.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 import 'package:provider/provider.dart';
 import 'package:rxdart/subjects.dart';
 
@@ -54,16 +56,19 @@ class LocatorPageState extends State<LocatorPage> {
               image: AssetImage("assets/images/background1300.png"),
               fit: BoxFit.fill),
         ),
-        child: Container(
-          child: Stack(
-            children: <Widget>[
-              AppBar(
-                backgroundColor: Colors.transparent,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 36.0),
-                child: Container(
-                  margin: EdgeInsets.only(top: 64.0, right: 24.0, left: 24.0),
+        child: SafeArea(
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: const Text('Exit'),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            body: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 24.0, right: 24.0, left: 24.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     gradient: LinearGradient(
@@ -85,25 +90,28 @@ class LocatorPageState extends State<LocatorPage> {
                     ),
                   ),
                 ),
-              ),
-              Container(
-                  child: searchResult != null
-                      ? buildSuggestionList(searchResult)
-                      : Container()),
-              Container(
-                child: Card(
-                  margin: const EdgeInsets.only(
-                      top: 212.0, right: 24.0, left: 24.0),
-                  color: Colors.white,
-                  elevation: 10,
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(6))),
+                  margin:
+                  const EdgeInsets.only(top: 18.0, right: 24.0, left: 24.0),
                   child: Container(
-                    transform: Matrix4.translationValues(-18.0, 0.0, 0.0),
+                    transform: Matrix4.translationValues(-20.0, 0.0, 0.0),
                     child: TextField(
                       controller: _textController,
                       textAlign: TextAlign.start,
                       textAlignVertical: TextAlignVertical.center,
                       textDirection: TextDirection.ltr,
                       decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
                         prefixIcon: Container(
                           transform: Matrix4.translationValues(10.0, 0.0, 0.0),
                           child: Icon(
@@ -114,7 +122,6 @@ class LocatorPageState extends State<LocatorPage> {
                         ),
                         hintText: getHintText(),
                         fillColor: Colors.white,
-                        border: InputBorder.none,
                       ),
                       style: TextStyle(
                         fontSize: 13.0,
@@ -127,82 +134,55 @@ class LocatorPageState extends State<LocatorPage> {
                     ),
                   ),
                 ),
-              ),
-              Container(
-                  child: searchResult == null
-                      ? setUpButtonWithNoResults()
-                      : setUpButtonWithResults(searchResult)),
-            ],
+                Container(
+                    transform: Matrix4.translationValues(0.0, -8.0, 0.0),
+                    child: searchResult != null
+                        ? buildSuggestionList(searchResult)
+                        : Container()),
+                setupContinueButton(searchResult),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Container setUpButtonWithResults(DestinationSearchResult searchResult) {
-    return Container(
-      margin: searchResult.results.length == 0
-          ? EdgeInsets.only(top: 270.0, right: 24.0, left: 170.0)
-          : EdgeInsets.only(top: 448.0, right: 24.0, left: 170.0),
-      child: SizedBox(
-        width: double.infinity,
-        height: 40,
-        child: RaisedButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(getContinueButtonText(),
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w400)),
-              Icon(
-                Icons.chevron_right,
-                size: 20,
+  Flexible setupContinueButton(DestinationSearchResult searchResult) {
+    return Flexible(
+      child: Container(
+        margin: const EdgeInsets.only(top: 8.0, right: 24.0),
+        child: Align(
+          alignment: Alignment.topRight,
+          child: SizedBox(
+            width: 200,
+            height: 40,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(getContinueButtonText(),
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400)),
+                  Icon(
+                    Icons.chevron_right,
+                    size: 20,
+                  ),
+                ],
               ),
-            ],
+              color: Colors.pinkAccent,
+              textColor: Colors.white,
+              elevation: 4,
+              onPressed: () {
+                navigateToNextPage();
+              },
+            ),
           ),
-          color: Colors.pinkAccent,
-          textColor: Colors.white,
-          elevation: 4,
-          onPressed: () {
-            navigateToNextPage();
-          },
-        ),
-      ),
-    );
-  }
-
-  Container setUpButtonWithNoResults() {
-    return Container(
-      margin: EdgeInsets.only(top: 270.0, right: 24.0, left: 170.0),
-      child: SizedBox(
-        width: double.infinity,
-        height: 40,
-        child: RaisedButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(getContinueButtonText(),
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w400)),
-              Icon(
-                Icons.chevron_right,
-                size: 20,
-              ),
-            ],
-          ),
-          color: Colors.pinkAccent,
-          textColor: Colors.white,
-          elevation: 4,
         ),
       ),
     );
@@ -216,33 +196,35 @@ class LocatorPageState extends State<LocatorPage> {
     return Flexible(
       child: Visibility(
         visible: searchResult.results.length == 0 ? false : true,
-        child: Card(
-          margin: const EdgeInsets.only(
-              top: 250.0, right: 28.0, left: 28.0, bottom: 244.0),
-          color: Colors.white,
-          elevation: 8,
-          child: Container(
-            child: ListView.builder(
-              itemCount: searchResult.results.length,
-              itemBuilder: (BuildContext context, int index) => GestureDetector(
-                child: createSuggestionItemView(context, index, searchResult),
-                onTap: () async {
-                  final placeDetails = await _bloc
-                      .getPlaceDetails(searchResult.results[index].placeId);
-                  final latitude = placeDetails.geometry.location.lat;
-                  final longitude = placeDetails.geometry.location.lng;
-                  final address = placeDetails.formattedAddress;
-                  _addressEntered.add(address);
-                  _addressEntered.add(latitude);
-                  _addressEntered.add(longitude);
-                  FocusScope.of(context).unfocus();
-                  setState(() {
-                    routeData.destination = LatLng(latitude, longitude);
-                    _textController.text = placeDetails.formattedAddress;
-                    searchResult.results.clear();
-                  });
-                },
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.white,
               ),
+              borderRadius: BorderRadius.all(Radius.circular(6))),
+          margin: const EdgeInsets.only(right: 24.0, left: 24.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: searchResult.results.length,
+            itemBuilder: (BuildContext context, int index) => GestureDetector(
+              child: createSuggestionItemView(context, index, searchResult),
+              onTap: () async {
+                final placeDetails = await _bloc
+                    .getPlaceDetails(searchResult.results[index].placeId);
+                final latitude = placeDetails.geometry.location.lat;
+                final longitude = placeDetails.geometry.location.lng;
+                final address = placeDetails.formattedAddress;
+                _addressEntered.add(address);
+                _addressEntered.add(latitude);
+                _addressEntered.add(longitude);
+                FocusScope.of(context).unfocus();
+                setState(() {
+                  routeData.destination = LatLng(latitude, longitude);
+                  _textController.text = placeDetails.formattedAddress;
+                  searchResult.results.clear();
+                });
+              },
             ),
           ),
         ),
@@ -253,28 +235,20 @@ class LocatorPageState extends State<LocatorPage> {
   Widget createSuggestionItemView(
       BuildContext context, int index, DestinationSearchResult searchResult) {
     var placeSuggestion = searchResult.results[index];
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text(
-                      placeSuggestion.streetAddress +
-                          ", ${placeSuggestion.location}",
-                      style: TextStyle(
-                          fontSize: 12.0,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w400))
-                ],
-              ),
-            ],
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+                placeSuggestion.streetAddress + ", ${placeSuggestion.location}",
+                style: TextStyle(
+                    fontSize: 12.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w400)),
           ),
         ),
-      ),
+      ],
     );
   }
 
@@ -315,7 +289,6 @@ class LocatorPageState extends State<LocatorPage> {
                   TripConfirmationMap(routeData.origin, routeData.destination),
             ));
       } else if (routeData.destination == null) {
-
         //go to get home location
         Navigator.push(
             context,
@@ -325,7 +298,6 @@ class LocatorPageState extends State<LocatorPage> {
       }
     } else {
       if (routeData.destination != null) {
-
         // go to map
         Navigator.push(
             context,
