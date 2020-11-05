e3450187aa6f207be4023912428a49e1c100970b
Pearlson Omesa <pearlson.omesa@gmail.com>
2020-11-05 12:09:26 -0800

    fix bug where when you resume the app and click home it crashes or goes to a gray screen

--- lib/ui/pages/trip_confirmation_map.dart
+++ lib/ui/pages/trip_confirmation_map.dart
@@ -53,14 +53,19 @@ class TripConfirmationMapState extends State<TripConfirmationMap> {
           TripRouteResult result = snapshot.data;
           _goToStart(result.origin);
           _addMarkers(result.origin);
-          final coordinates = result.routePoints
-              .map((point) => LatLng(point.latitude, point.longitude))
-              .toList();
-          _polyline = Polyline(
-              polylineId: PolylineId("trip"),
-              color: Colors.red,
-              points: coordinates,
-              width: 5);
+
+          if (result.routePoints != null) {
+            final coordinates = result.routePoints
+                .map((point) => LatLng(point.latitude, point.longitude))
+                .toList();
+            _polyline = Polyline(
+                polylineId: PolylineId("trip"),
+                color: Colors.red,
+                points: coordinates,
+                width: 5);
+          } else {
+            _tripRouteBloc.fetchTripRoute(widget.destination, widget.origin);
+          }
         } else {
           _tripRouteBloc.fetchTripRoute(widget.destination, widget.origin);
         }
@@ -72,7 +77,7 @@ class TripConfirmationMapState extends State<TripConfirmationMap> {
   Future<void> _goToStart(LatLng start) async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(target: start, zoom: 12)));
+        CameraPosition(target: start, zoom: 10)));
   }
 
   Widget buildTripConfirmationView(BuildContext context,
